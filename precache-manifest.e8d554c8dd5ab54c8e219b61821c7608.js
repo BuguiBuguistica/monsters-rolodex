61386cac78b06c31eee043d417b222d641046d04
Bugarin, Valeria <Valeria.Bugarin@haufe.com>
2020-10-10 20:59:44 +0200

    Updates

---
+++ precache-manifest.e8d554c8dd5ab54c8e219b61821c7608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6e27789d11538f3e06d2024e504cc6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "60df62aec0f1cd2cd8b2",
+    "url": "/monsters-rolodex/static/css/main.e213814a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60df62aec0f1cd2cd8b2",
+    "url": "/monsters-rolodex/static/js/main.365f31a8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
